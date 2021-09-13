6f30a8d08fa1d79167de728d4ea598c949b6b497
deji adesoga <done2stay@gmail.com>
2021-09-13 13:04:44 +0100

    Added Functionalities

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from 'react-bootstrap'
+import Home from "./pages/Home";
+import ProductPage from './pages/ProductPage';
+import CartPage from './pages/CartPage';
+import ConfirmPage from './pages/ConfirmPage';
 
 function App() {
   return (
-    <>
-      <h1>Welcome to SuperMart</h1>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={Home} exact />
+          <Route path="/cart/:id?" component={CartPage} exact />
+          <Route path='/product/:id' component={ProductPage} />
+          <Route path='/confirm' component={ConfirmPage} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
